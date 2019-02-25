230a6efa5c00cbfd97451dc430b6dd8478091ee1
janetdejager <janetdejager@192.168.1.4>
2019-02-24 19:46:03 -0600

    Find Horizontal - look up columns and row.

--- WordSearchKata/src/main/java/jandjag/wordsearch/WordSearch.java
+++ WordSearchKata/src/main/java/jandjag/wordsearch/WordSearch.java
@@ -2,13 +2,25 @@ package jandjag.wordsearch;
 
 public class WordSearch {
 
-	int[] findHorizontal(String lineValue, String word) {
-		int[] returnValue = new int[word.length()];
-		 int begin = lineValue.indexOf(word);
-		 for (int i = 0; i < word.length(); i++) {
-			 returnValue[i] = begin + i;
-		 }
-		 return returnValue;
+	int[] findHorizontalColumns(char[] lineValue, String word) {
+		int begin = new String(lineValue).indexOf(word);
+		if (begin >= 0) {
+			int[] returnValue = new int[word.length()];
+			for (int i = 0; i < word.length(); i++) {
+				returnValue[i] = begin + i;
+			}
+			return returnValue;
+		}
+		return null;
+	}
+
+	int findHorizontalRow(char[][] rows, String word) {
+		for (int i = 0; i < rows.length; i++) {
+			if (findHorizontalColumns(rows[i], word) != null) {
+				return i;
+			}
+		}
+		return 0;
 	}
 
 }
--- WordSearchKata/src/test/java/jandjag/wordsearch/WordSearchTest.java
+++ WordSearchKata/src/test/java/jandjag/wordsearch/WordSearchTest.java
@@ -2,20 +2,42 @@ package jandjag.wordsearch;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class WordSearchTest {
+	
+	final static String WORD = "helloworld";
+	final static char[] LINE_WITH_WORD = ("abc" + WORD + "defgh").toCharArray();
+	final static char[] LINE_WITHOUT_WORD = "abcdefghijklmnopqrs".toCharArray();
+	WordSearch search = null;
 
+	@Before
+	public void setup() {
+		search = new WordSearch();
+	}
+	
 	/*
 	 * Tests around Horizontal Search
 	 */
 	@Test
-	public void shouldFindWordInHorizontalLine() {
-		WordSearch search = new WordSearch();
-		String word = "helloworld";
-		String lineValue = "abc" + word + "defgh";
-		int[] result = search.findHorizontal(lineValue, word);
+	public void shouldFindColumnsInHorizontal_OneLine() {
+		int[] result = search.findHorizontalColumns(LINE_WITH_WORD, WORD);
 		assertArrayEquals(new int[] {3,4,5,6,7,8,9,10,11,12}, result);
+	}
+	
+	@Test
+	public void shouldReturnNull_whenWordNotInRow() {
+		assertNull(search.findHorizontalColumns(LINE_WITHOUT_WORD, "zyx"));
+	}
+	
+	@Test
+	public void shouldFindRowInHorizontal_MultipleLines() {
+		char[][] rows = new char[3][];
+		rows[0] = LINE_WITHOUT_WORD;
+		rows[1] = LINE_WITH_WORD;
+		rows[2] = LINE_WITHOUT_WORD;
+		assertEquals(1, search.findHorizontalRow(rows, WORD));
 	}
 	
 }
