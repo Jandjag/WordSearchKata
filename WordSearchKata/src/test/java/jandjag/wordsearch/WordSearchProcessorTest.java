691d59db4ad09dfbc882d0ed15bddd8c45e1cc6e
janetdejager <janetdejager@192.168.1.9>
2019-03-31 17:25:29 -0500

    Adding another Quilting Search example.

--- WordSearchKata/src/test/java/jandjag/wordsearch/WordSearchProcessorTest.java
+++ WordSearchKata/src/test/java/jandjag/wordsearch/WordSearchProcessorTest.java
@@ -18,6 +18,11 @@ public class WordSearchProcessorTest {
 	public void processWordSearch_QuiltingExample() {
 		assertEquals(getQuiltingOutput(), WordSearchProcessor.processWordSearch(buildQuiltingContents()));
 	}
+
+	@Test
+	public void processWordSearch_QuiltingExample2() {
+		assertEquals(getQuiltingOutput2(), WordSearchProcessor.processWordSearch(buildQuiltingContents2()));
+	}
 	
 	private List<String> buildQuiltingContents() {
 		List<String> fileContents = new ArrayList<String>();
@@ -51,6 +56,36 @@ public class WordSearchProcessorTest {
 				"PIECING: (8,9),(8,8),(8,7),(8,6),(8,5),(8,4),(8,3)\n" + 
 				"QUILTING: (7,4),(6,4),(5,4),(4,4),(3,4),(2,4),(1,4),(0,4)\n";
 	}
+
+	private List<String> buildQuiltingContents2() {
+		List<String> fileContents = new ArrayList<String>();
+		
+		fileContents.add("BATIK,BINDING,CUTTING,FABRIC,PATTERN,PIECING,QUILTING");
+		fileContents.add("B,S,L,N,D,S,D,Q,V,O");
+		fileContents.add("G,V,V,R,C,C,U,O,D,G");
+		fileContents.add("G,N,I,D,N,I,B,J,W,D");
+		fileContents.add("R,F,I,V,L,D,J,B,B,X");
+		fileContents.add("D,P,A,T,T,E,R,N,B,T");
+		fileContents.add("Z,H,I,B,T,W,I,K,A,Y");
+		fileContents.add("J,N,T,Q,R,U,C,T,T,G");
+		fileContents.add("G,G,X,G,N,I,C,E,I,P");
+		fileContents.add("L,G,M,Y,I,L,C,H,K,G");
+		fileContents.add("L,M,I,B,P,L,N,M,L,X");
+		
+		return fileContents;
+	}
+
+	private String getQuiltingOutput2() {
+		return "BATIK: (8,4),(8,5),(8,6),(8,7),(8,8)\n" + 
+				"BINDING: (6,2),(5,2),(4,2),(3,2),(2,2),(1,2),(0,2)\n" + 
+				"CUTTING: (6,7),(5,6),(4,5),(3,4),(2,3),(1,2),(0,1)\n" + 
+				"FABRIC: (1,3),(2,4),(3,5),(4,6),(5,7),(6,8)\n" + 
+				"PATTERN: (1,4),(2,4),(3,4),(4,4),(5,4),(6,4),(7,4)\n" + 
+				"PIECING: (9,7),(8,7),(7,7),(6,7),(5,7),(4,7),(3,7)\n" + 
+				"QUILTING: (7,0),(6,1),(5,2),(4,3),(3,4),(2,5),(1,6),(0,7)\n";
+	}
+	
+	
 
 	private List<String> buildStarTrekContents() {
 		List<String> fileContents = new ArrayList<String>();
